784ed57038eee16718f83f9e91b00c2064e2d4c5
liviusceastlivii <70839080+liviusceastlivii@users.noreply.github.com>
2020-09-23 22:20:44 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,8 @@
+#lungimea laturii cubului=a
+a=5
+P=4*a
+Atutsupr=(a**2)*6
+V=a**3
+print(P)
+print(Atutsupr)
+print(V)
---
+++ 4.py
@@ -0,0 +1,6 @@
+n=6
+print(n*1000)
+print(n*1000000)
+print(n*12)
+print(n*52)
+print(n*365)
